20717c35b04a70fc2b07cfc3fe35794250ca3b88
Harry Saliba <harrysaliba4139@gmail.com>
2015-04-14 13:53:02 -0700

    Update for icons.

--- PokedexConstructor.java
+++ PokedexConstructor.java
@@ -6,10 +6,17 @@ public class PokedexConstructor {
         String data;
         String[] attr = new String[10];
         Pokemon newPkmn = new Pokemon();
+        List<byte> imageBytes = new ArrayList<byte>;
+        FileInputStream fis;
+        byte iByte = null;
         while ((data = br.readLine()) != null) {
             attr = data.substring(1, data.length() - 1).split("/");
+            fis = new FileInputStream("resources/icons/bulbasuar.png");
+            while ((iByte = fis.read()) != -1) {
+                imageBytes.add(iByte);
+            }
             //Add a byte[] for image data in Pokemon.class and a int getImageSize for the array also set the byte[] data
-            newPkmn.set(attr[0], attr[1], attr[2], "NA", "NA", "NA", "NA");
+            newPkmn.set(attr[0], attr[1], attr[2], "NA", "NA", "NA", "NA", imageBytes);
             saveObject(attr[1], newPkmn);
         }
     }
