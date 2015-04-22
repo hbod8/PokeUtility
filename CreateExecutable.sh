e81ad1bfbfebcf992127d2975aae6ac339599da7
Harry Saliba <harrysaliba4139@gmail.com>
2015-04-21 20:13:43 -0700

    Testing.

--- CreateExecutable.sh
+++ CreateExecutable.sh
@@ -1,4 +1,3 @@
 javac PokeUtility.java
 jar -cvfm PokeUtility.jar manifest Pokemon.class PokeUtility.class Search.class PokeUtilityGUI.class SearchInput.class resources/pokedex/* resources/PressStart2P.ttf
-rm *.class
 echo "Made excecutable: PokeUtility.jar"
