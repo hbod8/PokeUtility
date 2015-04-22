1107f7f5e48b5f459a72a85f1fa2460fd350e520
Harry Saliba <harrysaliba4139@gmail.com>
2015-04-21 20:57:00 -0700

    Fixed a bug.

--- CreateExecutable.sh
+++ CreateExecutable.sh
@@ -1,3 +1,4 @@
+cd "$(dirname "$0")"
 javac PokeUtility.java
 jar -cvfm PokeUtility.jar manifest Pokemon.class PokeUtility.class Search.class PokeUtilityGUI.class SearchInput.class resources/pokedex/* resources/PressStart2P.ttf
 echo "Made excecutable: PokeUtility.jar"
